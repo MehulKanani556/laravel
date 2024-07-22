d7226257eaecb004e2a363d98ef8351b1b31ccdc
Mehul Kanani <mkanani556@gmail.com>
2024-07-22 22:51:17 +0530

    range , checkbox , radio , select field in form

--- app/Http/Controllers/UserForm.php
+++ app/Http/Controllers/UserForm.php
@@ -9,12 +9,25 @@ use Illuminate\Http\Request;
 class UserForm extends Controller
 {
     //
+    // function addUser(Request $req){
+    //     echo "User Name is $req->username";
+    //     echo "<br>";
+    //     echo "User Email is $req->email";
+    //     echo "<br>";
+    //     echo "User City is $req->city";
+    //     // return $req;
+    // }
     function addUser(Request $req){
-        echo "User Name is $req->username";
-        echo "<br>";
-        echo "User Email is $req->email";
-        echo "<br>";
-        echo "User City is $req->city";
+        echo $req->gender;
+        echo "<br />";
+        
+        echo $req->city;
+        echo "<br />";
+        echo $req->age;
+        echo "<br />";
+        print_r ($req->skill);
+      
+
         // return $req;
     }
 }
--- resources/views/user-form.blade.php
+++ resources/views/user-form.blade.php
@@ -1,6 +1,6 @@
 <div>
     <h2>Form </h2>
-    <form action="addUser" method="post">
+    {{-- <form action="addUser" method="post">
         @csrf
         <div class="input-wrapper">
             <input type="text" placeholder="Enter User Name" name="username">
@@ -14,11 +14,49 @@
         <div class="input-wrapper">
             <button>Add New User</button>
         </div>
+        
+    </form> --}}
+    <hr>
+    <form action="addUser" method="post">
+        @csrf
+        <div>
+            <h5>User Skill</h5>
+            <input type="checkbox" name="skill[]" id="php" value="PHP">
+            <label for="php">PHP</label>
+            <input type="checkbox" name="skill[]" id="noe" value="Node">
+            <label for="node">Node</label>
+            <input type="checkbox" name="skill[]" id="java" value="Java">
+            <label for="java">Java</label>
+        </div>
+        <div>
+            <h5>Gender</h5>
+            <input type="radio" name="gender" id="male" value="male">
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="female">
+            <label for="female">Female</label>
+        </div>
+        <div>
+            <h5>City</h5>
+            <select name="city" id="">
+                <option value="">Select City</option>
+                <option value="Delhi">Delhi</option>
+                <option value="Mumbai">Mumbai</option>
+                <option value="Kolkata">Kolkata</option>
+                
+            </select>
+        </div>
+        <div>
+            <h5>Age</h5>
+            <div>
+                <input type="range" name="age" id="age" min="18" max="100">
+            </div>
+            <button>Add New User</button>
+        </div>
     </form>
 </div>
 
 <style>
-    input,button{
+    /* input,button{
         border: orange 1px solid;
         height: 35px;
         width: 200px;
@@ -32,5 +70,5 @@
     button{
         background-color: white;
         cursor: pointer;
-    }
+    } */
 </style>
